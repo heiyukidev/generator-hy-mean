1c417e6e2259e38c389dd7052b96a9c32f1fc3ee
Khaled Romdhane <heiyukidev@gmail.com>
2017-02-17 15:36:05 +0100

    deleting test folder

--- sexy/app/components/indexTitle/indexTitle.controller.js
+++
@@ -1,7 +0,0 @@
-(function(angular) {
-  function controllerFn($scope) {
-    $scope.message = " Hello There!";
-  }
-  controllerFn.$inject = ['$scope'];
-  angular.module('indexTitle').controller('IndexTitleController', controllerFn);
-})(angular);
--- sexy/app/pages/indexPage/index.controller.js
+++
@@ -1,7 +0,0 @@
-(function(angular) {
-  function controllerFn() {
-
-  }
-  controllerFn.$inject = [];
-  angular.module('index').controller('IndexController', controllerFn);
-})(angular);
